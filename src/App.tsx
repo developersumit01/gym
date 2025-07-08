0eb0fc5f5bac0ddf3584d70c7ed62ed214b9e682
Sumit Kumar <sumitkumar84390@gmail.com>
2025-07-08 15:47:03 +0530

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App=()=>{
+  return (
+    <div className="App">
+      <h1>Welcome to the GYM App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App
