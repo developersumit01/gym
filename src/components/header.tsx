4d8ecf82e0894015edb991b8ecdf6c8a5338c3fa
Sumit Kumar <sumitkumar84390@gmail.com>
2025-07-09 20:29:14 +0530

    Header done for both mobile and pc

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "../assets/images/logo.png";
 import HamburgerIcon from "../assets/icons/hamburger.svg";
+import { useState } from "react";
 const Header = () => {
+   const [ isHeaderOpen, setIsHeaderOpen ] = useState<boolean>(false);
    return (
       <>
          <header className="flex justify-between items-center p-2 bg-background text-text">
@@ -9,30 +11,30 @@ const Header = () => {
                <div className="w-12">
                   <NavLink to={"/"}><img src={Logo} alt="" /></NavLink>
                </div>
-               <nav className="hidden md:flex space-x-6 text-lg font-semibold">
-                  <NavLink to="">Home</NavLink>
-                  <NavLink to="">About</NavLink>
-                  <NavLink to="">Workout Programs</NavLink>
-                  <NavLink to="">Recipes</NavLink>
-                  <NavLink to="">Store</NavLink>
-                  <NavLink to="">Login</NavLink>
-                  <NavLink to="">Sign up</NavLink>
+               <nav className="hidden md:flex space-x-4 text-lg font-semibold">
+                  <NavLink className="px-2" to="/">Home</NavLink>
+                  <NavLink className="px-2" to="/about">About</NavLink>
+                  <NavLink className="px-2" to="/workout-programs">Workout Programs</NavLink>
+                  <NavLink className="px-2" to="/recipes">Recipes</NavLink>
+                  <NavLink className="px-2" to="/store">Store</NavLink>
+                  <NavLink className="px-2" to="/login">Login</NavLink>
+                  <NavLink className="px-2" to="/sign-up">Sign up</NavLink>
                </nav>
             </div>
             <div className="md:hidden">
-               <button className="w-10 h-10 flex justify-center items-center">
+               <button onClick={() => { setIsHeaderOpen(!isHeaderOpen) }} className="w-10 h-10 flex justify-center items-center">
                   <img src={HamburgerIcon} alt="Menu" />
                </button>
             </div>
          </header>
-         <header className="flex md:hidden justify-between items-start px-2 bg-background text-text font-semibold flex-col">
-            <NavLink to="" className="w-full border-y py-2 pl-2 focus-bg-fill">Home</NavLink>
-            <NavLink to="" className="w-full border-b py-2 pl-2 focus-bg-fill">About</NavLink>
-            <NavLink to="" className="w-full border-b py-2 pl-2 focus-bg-fill">Workout Programs</NavLink>
-            <NavLink to="" className="w-full border-b py-2 pl-2 focus-bg-fill">Recipes</NavLink>
-            <NavLink to="" className="w-full border-b py-2 pl-2 focus-bg-fill">Store</NavLink>
-            <NavLink to="" className="w-full border-b py-2 pl-2 focus-bg-fill">Login</NavLink>
-            <NavLink to="" className="w-full border-b py-2 pl-2 focus-bg-fill">Sign up</NavLink>
+       <header className={`flex flex-col fixed w-full md:hidden justify-between items-start px-2 bg-background text-text font-semibold overflow-hidden transition-all duration-300 ease-in-out ${isHeaderOpen ? 'max-h-[1000px] opacity-100 translate-y-0' : 'max-h-0 opacity-0 -translate-y-2'}`}>
+            <NavLink to="/" className="w-full border-y py-2 pl-2 focus-bg-fill">Home</NavLink>
+            <NavLink to="/about" className="w-full border-b py-2 pl-2 focus-bg-fill">About</NavLink>
+            <NavLink to="/workout-programs" className="w-full border-b py-2 pl-2 focus-bg-fill">Workout Programs</NavLink>
+            <NavLink to="/recipes" className="w-full border-b py-2 pl-2 focus-bg-fill">Recipes</NavLink>
+            <NavLink to="/store" className="w-full border-b py-2 pl-2 focus-bg-fill">Store</NavLink>
+            <NavLink to="/login" className="w-full border-b py-2 pl-2 focus-bg-fill">Login</NavLink>
+            <NavLink to="/sign-up" className="w-full border-b py-2 pl-2 focus-bg-fill">Sign up</NavLink>
          </header>
       </>
    )
