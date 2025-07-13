3db5d6bfea0a0ca5a89493cac0c7a34e564dc32e
Sumit Kumar <sumitkumar84390@gmail.com>
2025-07-13 13:35:44 +0530

    Product list page done

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -9,9 +9,9 @@ const ProductCard: React.FC<ProductCardProps> = ({ id,name, image }) => {
    return (
       <React.Fragment key={id}> 
          {/* Product Card */}
-         <div className="flex justify-center h-96">
+         <div className="flex justify-center h-80">
             <div className="relative h-full rounded-lg min-w-sm flex flex-col justify-center">
-               <img src={image} alt="" className="relative z-20 w-80 h-60" />
+               <img src={image} alt="" className="relative z-20 w-80 h-60 object-contain mb-4" />
                <div className="text-lg font-semibold relative z-20 pb-2">
                   {name}
                </div>
